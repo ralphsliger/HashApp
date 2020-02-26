68684df737dffc551c63a7bdcbf075f3b55c8d58
Pablo Arrieta <pablou2015@hotmail.com>
2020-02-25 23:37:40 -0500

    hash and json table

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
@@ -9,23 +9,30 @@ bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/data.db'
 db = SQLAlchemy(app)
 
-#modelo base de datos
+# modelo base de datos
+
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(256))
 
+
 @app.route('/chain', methods=['GET'])
 def home():
-    tasks = Task.query.all() #mostrar listado completo en home
-    return render_template('index.html', tasks = tasks)
+
+    tasks = Task.query.all()  # mostrar listado completo en home.
+
+    return render_template('index.html', tasks=tasks)
 
 
 @app.route('/chain', methods=['POST'])
 def create():
-    task = Task(content=request.form['content'])  #recibe parametro content del formulario y lo guarda en base de datos
+    # recibe parametro content del formulario y lo guarda en base de datos
+    task = Task(content=generate_password_hash(request.form['content']))
     db.session.add(task)
     db.session.commit()
     return redirect(url_for('home'))
+
 
 @app.route('/chain/last', methods=['GET'])
 def last():
@@ -35,7 +42,18 @@ def last():
 
 @app.route('/api/v1/chain/', methods=['GET'])
 def json():
-    return jsonify({'Json': Task.query.all()})
+    tasks = Task.query.all()  # mostrar listado completo en home.
+    tasks_json = []
+    for i in range(0, len(tasks)-1):
+        aux = {
+            'index': int(i)+1,
+            'hash': tasks[i].content,
+        }
+        tasks_json.append(aux)
+
+    return render_template('index2.html', tasks=tasks_json)
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)  # host defecto flask, puerto 8000
+    # host defecto flask, puerto 8000
+    app.run(host="0.0.0.0", port=8000, debug=True)
